dd3eafe3f0ef34df436b3c6bd2aaef8aef429d56
Mikko Salminen <115707134+Peresteks@users.noreply.github.com>
2024-05-29 20:20:56 +0300

    Delete mymodel.js

--- mymodel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const firstschema = new mongoose.Schema({
-  // Määritellään kentät ja niiden tyypit
-});
-
-const firstmodel = mongoose.model('firstmodel', firstschema);
-
-module.exports = firstmodel;
